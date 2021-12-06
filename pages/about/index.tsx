ccaac881e94aa18eecc6edb7a8cd3e62e5988586
Nicolas Suarez <nicolas.suarez@nivelics.co>
2021-12-06 10:52:02 -0500

    first commit

---
+++ pages/about/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AboutComponent from './about'
+
+const AboutPage = () => {
+  return (
+    <div>
+      <AboutComponent />
+    </div>
+  )
+}
+
+export default AboutPage
